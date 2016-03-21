6d70ea1fef487ca56e79de79f7ed8311855e80b4
Evan Denerley <EvanDenerley@gmail.com>
2016-03-21 13:15:55 -0400

    Make the app run

--- app/src/main/java/com/example/android/sunshine/app/gcm/MyGcmListenerService.java
+++ app/src/main/java/com/example/android/sunshine/app/gcm/MyGcmListenerService.java
@@ -31,25 +31,18 @@ import com.example.android.sunshine.app.MainActivity;
 import com.example.android.sunshine.app.R;
 import com.google.android.gms.gcm.GcmListenerService;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class MyGcmListenerService extends GcmListenerService {
 
+    public static final int NOTIFICATION_ID = 1;
     private static final String TAG = "MyGcmListenerService";
-
-    private static final String EXTRA_DATA = "data";
     private static final String EXTRA_WEATHER = "weather";
     private static final String EXTRA_LOCATION = "location";
-
-    public static final int NOTIFICATION_ID = 1;
 
     /**
      * Called when message is received.
      *
      * @param from SenderID of the sender.
-     * @param data Data bundle containing message data as key/value pairs.
-     *             For Set of keys use data.keySet().
+     * @param data Data bundle containing message data as key/value pairs. For Set of keys use data.keySet().
      */
     @Override
     public void onMessageReceived(String from, Bundle data) {
@@ -64,15 +57,15 @@ public class MyGcmListenerService extends GcmListenerService {
             if ((senderId).equals(from)) {
                 // Process message and then post a notification of the received message.
                 try {
-                    JSONObject jsonObject = new JSONObject(data.getString(EXTRA_DATA));
-                    String weather = jsonObject.getString(EXTRA_WEATHER);
-                    String location = jsonObject.getString(EXTRA_LOCATION);
+                    String weather = data.getString(EXTRA_WEATHER);
+                    String location = data.getString(EXTRA_LOCATION);
                     String alert =
-                            String.format(getString(R.string.gcm_weather_alert), weather, location);
+                        String.format(getString(R.string.gcm_weather_alert), weather, location);
                     sendNotification(alert);
-                } catch (JSONException e) {
+                } catch (Exception e) {
                     // JSON parsing failed, so we just let this message go, since GCM is not one
                     // of our critical features.
+                    Log.e(TAG, String.format("Error parsing JSON data: %1$s", e));
                 }
             }
             Log.i(TAG, "Received: " + data.toString());
@@ -80,16 +73,15 @@ public class MyGcmListenerService extends GcmListenerService {
     }
 
     /**
-     *  Put the message into a notification and post it.
-     *  This is just one simple example of what you might choose to do with a GCM message.
+     * Put the message into a notification and post it. This is just one simple example of what you might choose to do with a GCM message.
      *
      * @param message The alert message to be posted.
      */
     private void sendNotification(String message) {
         NotificationManager mNotificationManager =
-                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
         PendingIntent contentIntent =
-                PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
+            PendingIntent.getActivity(this, 0, new Intent(this, MainActivity.class), 0);
 
         // Notifications using both a large and a small icon (which yours should!) need the large
         // icon as a bitmap. So we need to create that here from the resource ID, and pass the
@@ -97,13 +89,13 @@ public class MyGcmListenerService extends GcmListenerService {
         // small icon, so that users understand what app is triggering this notification.
         Bitmap largeIcon = BitmapFactory.decodeResource(this.getResources(), R.drawable.art_storm);
         NotificationCompat.Builder mBuilder =
-                new NotificationCompat.Builder(this)
-                        .setSmallIcon(R.drawable.art_clear)
-                        .setLargeIcon(largeIcon)
-                        .setContentTitle("Weather Alert!")
-                        .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
-                        .setContentText(message)
-                        .setPriority(NotificationCompat.PRIORITY_HIGH);
+            new NotificationCompat.Builder(this)
+                .setSmallIcon(R.drawable.art_clear)
+                .setLargeIcon(largeIcon)
+                .setContentTitle("Weather Alert!")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(message))
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_HIGH);
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
     }
